4dd9db0ec55e244f8b09ceb3415af386a3eaf5fb
terrafirma2021 <69117226+terrafirma2021@users.noreply.github.com>
2024-02-25 01:31:58 +0000

    Update main.cpp

--- YDS_datalogger/src/main.cpp
+++ YDS_datalogger/src/main.cpp
@@ -5,15 +5,15 @@
 #include <yds.h>
 
 // Definition for L9637D pins
-#define DASH_RX 1
-#define DASH_TX 38 // Left for future use
+#define DASH_TX 1
+#define DASH_RX 38 // Left for future use
+
 
 void setup() {
+    delay(1000);
     Serial.begin(115200);
     Serial1.begin(16040, DASH_RX, DASH_TX);
-    Serial.println("Yamaha ESP32 S3 BLE elm327 Emulator");
-    delay(1000);
-
+    Serial.println("Yamaha ESP32 S3 BLE elm327 Emulator"); 
     MyCallbacks *myCallbacks = new MyCallbacks();
     bool success = Device::getInstance().start(myCallbacks);
     if (!success) {
@@ -22,19 +22,17 @@ void setup() {
 }
 
 // Function to read and process serial data, and handle UART data
-void readAndProcessSerialData()
-{
-  while (Serial.available())
-  {
-    byte incomingByte = Serial.read();
+void readAndProcessSerialData() {
+  if (Serial1.available()) {
+    byte incomingByte = Serial1.read();
     processSerialByte(incomingByte);
+    //Indicate that data has been received
+   // Serial.println("Data Received");
   }
 }
-
 
 void loop()
 {
   handleBikeOffCondition();
   readAndProcessSerialData();
 }
-
