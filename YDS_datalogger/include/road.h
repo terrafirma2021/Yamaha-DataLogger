70d20087414c241d61bfa4df7288a63d8c961aba
terrafirma2021 <69117226+terrafirma2021@users.noreply.github.com>
2024-03-26 21:42:55 +0000

    Start of migration

---
+++ YDS_datalogger/include/road.h
@@ -0,0 +1,15 @@
+#ifndef _ROAD_H
+#define _ROAD_H
+
+#include <string>
+
+
+class Road
+{
+public:
+    static void toUpperCaseInPlace(std::string &);
+    static void trimInPlace(std::string &);
+};
+
+
+#endif // _ROAD_H
